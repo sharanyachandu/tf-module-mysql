88a629b9c0a8c916969d487de3509c10615730ed
sharanya chandu <you@example.com>
2023-06-30 00:20:32 +0530

    auto save

--- schema.tf
+++ schema.tf
@@ -1,14 +1,15 @@
-resource "aws_instance" "schema" {
- 
- 
-  provisioner "local-exec" {
-    command = <<EOF
-    cd /tmp
-    curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-    unzip mysql.zip
-    cd mysql-main
-    mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 < shipping.sql
-    EOF
-  }
+# This is going to inject the MySQL Schema
+resource "null_resource" "schema" {
 
+       depends_on = [aws_db_instance.mysql]
+
+  provisioner "local-exec" {
+        command = <<EOF
+        cd /tmp 
+        curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
+        unzip -o mysql.zip 
+        cd mysql-main 
+        mysql -h ${aws_db_instance.mysql.address} -uadmin1 -pRoboShop1 < shipping.sql
+        EOF
+    }
 }
