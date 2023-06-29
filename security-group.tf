c62772a18ab41183738092a2fa9418e2ac2f3615
sharanya chandu <you@example.com>
2023-06-29 23:48:35 +0530

    auto save

--- security-group.tf
+++ security-group.tf
@@ -6,23 +6,6 @@ resource "aws_security_group" "allow_mysql" {
 
   ingress {
     description      = "Allows docdb from local network"
-    from_port        = 3306
-    to_port          = 3306
-    protocol         = "tcp"
-    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
-  }
-
-  ingress {
-    description      = "Allows docdb from default network"
-    from_port        = 3306
-    to_port          = 3306# Creates Security Group 
-resource "aws_security_group" "allow_mysql" {
-  name               = "robot-${var.ENV}-mysql-sg"
-  description        = "Allows mysql Internal inbound traffic"
-  vpc_id             = data.terraform_remote_state.vpc.outputs.VPC_ID
-
-  ingress {
-    description      = "Allows docdb from local network"
     from_port        = var.MYSQL_RDS_PORT
     to_port          = var.MYSQL_RDS_PORT
     protocol         = "tcp"
@@ -33,21 +16,6 @@ resource "aws_security_group" "allow_mysql" {
     description      = "Allows docdb from default network"
     from_port        = var.MYSQL_RDS_PORT
     to_port          = var.MYSQL_RDS_PORT
-    protocol         = "tcp"
-    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "robot-${var.ENV}-mysql-sg"
-  }
-}
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
